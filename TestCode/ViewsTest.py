2eb7ad97d613c9c5b4cc1c7fda48bebbcb30e610
Minhajul Abedin <minhajul.abedin.rd@gmail.com>
2021-05-15 20:08:50 +0600

    Add unittest for views method: add, increase, decrease, remove cart

--- TestCode/ViewsTest.py
+++ TestCode/ViewsTest.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import AnonymousUser
 
 import mock
 
+from django.contrib.messages.storage.fallback import FallbackStorage
+
 
 _views = __import__("MVC Structure.Controller.views")
 _views = _views.Controller.views
@@ -11,14 +13,6 @@ _views = _views.Controller.views
 _models = __import__("MVC Structure.Model.models")
 _models = _models.Model.models
 
-# cursor_wrapper = mock.Mock()
-# cursor_wrapper.side_effect = RuntimeError("No touching the database!")
-#
-#
-# @mock.patch(
-#     "django.db.backends.util.CursorWraper",
-#     cursor_wrapper
-# )
 
 class ViewsTest(TestCase):
     def setUp(self):
@@ -36,18 +30,66 @@ class ViewsTest(TestCase):
         self.product.old_price = 120000
         self.product.price = 100000
         self.product.save()
-    def testLogin(self):
-        request = self.factory.get('/account/login')
+
+        self.request = self.factory.get('/shop/add/1')
+        self.request.user = self.user
+        setattr(self.request, 'session', 'session')
+        messages = FallbackStorage(self.request)
+        setattr(self.request, '_messages', messages)
+        self.response = _views.add_to_cart(self.request,1)
+
+
+    def test_add_to_cart(self):
+
+        cart = _models.Cart.objects.get(user=self.user)
+        self.assertEqual(cart.quantity, 1, "quantity of an item should be 1 at first time.")
+
+        self.response = _views.add_to_cart(self.request,1)
+        self.response = _views.add_to_cart(self.request,1)
+        self.response = _views.add_to_cart(self.request,1)
+
+        cart = _models.Cart.objects.get(user=self.user)
+        self.assertEqual(self.response.status_code, 302, "Should get a successfull response.")
+        self.assertEqual(cart.quantity, 4, "Same product should add to the same cart 3 time.")
+        self.assertFalse(cart.purchased, "Purchased should be false by default.")
+        self.assertEqual(cart.item.name, "Iphone 12 pro max", "Should add the 1st product in the cart.")
+
+    def test_increase_cart(self):
+        request = self.factory.get('/shop/increase/1')
         request.user = self.user
-        response = _views.login_user(request)
-        # print(response.content)
-        # print(help(response))
-        # print(help(response))
-        # response = MyView.as_View()(request)
-        self.assertEqual(response.status_code, 200)
-    # def test_login_post(self):
-    #     pass
-        # request = self.factory.post('/account/login',{'username':"test@gmail.com", "password":"top_secret"})
-        # expected_string = "You Are Logged in Successfully"
-        # self.assertContains(response.content, expected_string, "Should have logged in with correct credential")
-        # self.assertEqual(response.status_code, 200)
+        setattr(request, 'session', 'session')
+        messages = FallbackStorage(request)
+        setattr(request, '_messages', messages)
+        response = _views.increase_cart(request,1)
+        cart = _models.Cart.objects.get(user=request.user)
+
+        self.assertEqual(response.status_code, 302, "Should get a successfull response.")
+        self.assertEqual(cart.quantity, 2, "quantity of an item should be zero at first time.")
+        self.assertFalse(cart.purchased, "Purchased should be false by default.")
+        self.assertEqual(cart.item.name, "Iphone 12 pro max", "Should add the 1st product in the cart.")
+
+    def test_decrease_item(self):
+        response = _views.add_to_cart(self.request,1)
+        request = self.factory.get('/shop/decrease/1')
+        request.user = self.user
+        setattr(request, 'session', 'session')
+        messages = FallbackStorage(request)
+        setattr(request, '_messages', messages)
+        response = _views.decrease_item(request,1)
+        cart = _models.Cart.objects.get(user=request.user)
+
+        self.assertEqual(response.status_code, 302, "Should get a successfull response.")
+        self.assertEqual(cart.quantity, 1, "quantity of an item should be zero at first time.")
+        self.assertFalse(cart.purchased, "Purchased should be false by default.")
+        self.assertEqual(cart.item.name, "Iphone 12 pro max", "Should add the 1st product in the cart.")
+
+    def test_remove_from_cart(self):
+        request = self.factory.get('/shop/remove/1')
+        request.user = self.user
+        setattr(request, 'session', 'session')
+        messages = FallbackStorage(request)
+        setattr(request, '_messages', messages)
+        response = _views.remove_from_cart(request,1)
+        order_item = _models.Cart.objects.filter(item=self.product, user=request.user, purchased=False)
+        self.assertEqual(response.status_code, 302, "Should get a successfull response.")
+        self.assertEqual(len(order_item), 0 , "Cart should be remove from order")
